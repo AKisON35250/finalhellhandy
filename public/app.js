0453e81a1c8e041beec24cec2bf6a1f4b1e0d833
AKisON35250 <akison089@gmail.com>
2026-02-08 01:36:46 +0100

    Add files via upload

---
+++ public/app.js
@@ -0,0 +1,33 @@
+async function login() {
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+
+  const res = await fetch("/login",{
+    method:"POST",
+    headers:{"Content-Type":"application/json"},
+    body:JSON.stringify({username,password})
+  });
+  const data = await res.json();
+
+  if(data.success){
+    document.getElementById("lockscreen").style.display="none";
+    document.getElementById("phoneUI").classList.remove("hidden");
+    localStorage.setItem("userId",data.userId);
+  } else {
+    alert(data.message);
+  }
+}
+
+async function register() {
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+
+  const res = await fetch("/register",{
+    method:"POST",
+    headers:{"Content-Type":"application/json"},
+    body:JSON.stringify({username,password})
+  });
+  const data = await res.json();
+  if(data.success) alert("Registriert! Jetzt Login");
+  else alert(data.message);
+}
